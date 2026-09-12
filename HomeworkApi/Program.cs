eb92be7bd3d02f30360aec6b747e68d5efc0b27d
PLuck <saskatarov@gmail.com>
2026-09-12 18:16:44 +0700

    Apply migrations on startup

--- HomeworkApi/Program.cs
+++ HomeworkApi/Program.cs
@@ -22,6 +22,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 app.Urls.Add($"http://+:{port}");
 
