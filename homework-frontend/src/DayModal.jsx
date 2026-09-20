5a3d2841dcf90a90acb5bdd16f76965b41014da3
PLuck <saskatarov@gmail.com>
2026-09-20 16:47:23 +0700

    Add missing toLocalDateString import

--- homework-frontend/src/DayModal.jsx
+++ homework-frontend/src/DayModal.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import api from './api';
 import { useAuth } from './AuthContext';
+import { toLocalDateString } from './utils/date';
 
 const DayModal = ({ day, homeworks, onClose, onUpdate }) => {
     const { user } = useAuth();
